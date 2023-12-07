487aaa1b7fcd5f24a2f6ce649c459af8ef94b7df
AKD <contact.mousom@gmail.com>
2023-12-07 21:30:07 +0600

    Counter function is added

--- src/components/counter.js
+++ src/components/counter.js
@@ -6,13 +6,13 @@ const Counter = () => {
 
     const [count, setCount] = useState(0)
 
-    // const increase = () =>{
-    //    setCount(count + 1)
-    // }
+    const increase = () =>{
+       setCount(count + 1)
+    }
 
-    // const decrease = () =>{
-    //     setCount(count - 1)
-    // }
+    const decrease = () =>{
+        setCount(count - 1)
+    }
 
     return (
         <div >
